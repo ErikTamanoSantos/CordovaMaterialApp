7af440e3efbe0db2acfc14855cf0afabe59697d3
Erik Tamano <eriktamano@gmail.com>
2023-02-03 18:53:06 +0100

    tabs afegits

--- www/js/init.js
+++ www/js/init.js
@@ -2,6 +2,9 @@
   $(function(){
 
     $('.sidenav').sidenav();
-
+    $(document).ready(function(){
+      $('.tabs').tabs();
+      $('.tabs').tabs({'swipeable':true})
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
