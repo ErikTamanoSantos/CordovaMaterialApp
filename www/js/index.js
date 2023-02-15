9b5a85eb9bb88cf276244847b3b97a2e516ffe4d
Erik Tamano <eriktamano@gmail.com>
2023-02-15 21:12:41 +0100

    camera added

--- www/js/index.js
+++ www/js/index.js
@@ -22,7 +22,80 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+
+    $('.sidenav').sidenav();
+    M.Tabs.init($('.tabs'), {swipeable: true})
+    M.Tabs.getInstance($(".tabs")).select('div-tab-1')
+    $("#loadArticlesButton").click(loadArticles)
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+
+    $("#cameraButton").click(function() {
+        let cameraOptions = {}
+        navigator.camera.getPicture(cameraSuccess, cameraError, cameraOptions)
+    })
+
+
+}
+
+function cameraSuccess(imageData) {
+    let image = document.getElementById("image")
+    image.src = "data:image/jpeg;base64," + imageData
+}
+
+function cameraError(message) {
+    alert(message)
+}
+
+function loadArticles() {
+
+    $(".collection-item").remove()
+    $.ajax({
+        method: "GET",
+        url: "https://api.spaceflightnewsapi.net/v3/articles?_limit=8",
+        dataType: "json",   // necessitem això pq ens retorni un objecte JSON
+    }).done(function (msg) {
+        for(let item in msg) {
+            console.log(msg[item]);
+            title = msg[item].title
+
+            let html = `
+            <div class="col s12 m6 l3">
+            <div class="card">
+            <div class="card-image">
+            <img src="${msg[item].imageUrl}">
+            <span class="card-title">${msg[item].title}</span>
+            </div>
+            <div class="card-content">
+            <p>${msg[item].summary}</p>
+            </div>
+            <div class="card-action">
+            <a href="${msg[item].url}">Full article</a>
+            </div>
+            </div>
+            </div>`
+
+            $("#div-tab-3 .row").append(html)
+            $("#articleList").append(`<li class="collection-item" id="${item}"><div>${title}<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>`)
+            $(`#${item}`).off().on("click", function() {
+                let html = `
+                <div class="card-image">
+                <img src="${msg[item].imageUrl}">
+                <span class="card-title">${msg[item].title}</span>
+                </div>
+                <div class="card-content">
+                <p>${msg[item].summary}</p>
+                </div>
+                <div class="card-action">
+                <a href="${msg[item].url}">Full article</a>
+                </div>
+                `
+                $("#articleCard").html(html)
+
+                M.Tabs.getInstance($(".tabs")).select('div-tab-2')
+            })
+        };
+    }).fail(function () {
+        alert("ERROR");
+    });
 }
